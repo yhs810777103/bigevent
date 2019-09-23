3e698c257d43e679bc87ba34be13999751c0b45a
yhs810777103 <810777103@qq.com>
2019-09-23 21:34:33 +0800

    显示添加编辑删除功能

--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -1,6 +1,12 @@
 var BASE_URL='http://localhost:8000';
 var URLIST={
+    // 管理员方面接口地址
     user_login:BASE_URL+'/admin/login',
     user_logout:BASE_URL+'/admin/logout',
-    user_logInfo:BASE_URL+'/admin/getuser'
+    user_logInfo:BASE_URL+'/admin/getuser',
+    // 文章类别接口地址
+    category_show:BASE_URL+'/admin/category_search',
+    category_add:BASE_URL+'/admin/category_add',
+    category_del:BASE_URL+'/admin/category_delete',
+    category_edit:BASE_URL+'/admin/category_edit',
 }
